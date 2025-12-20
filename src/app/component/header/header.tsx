ff17f11c6398ec0f0e1d08be912bdcd1c27a7ebb
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-12-20 19:07:14 +0600

    header menu add

--- src/app/component/header/header.tsx
+++ src/app/component/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Header() {
+  const navMenus=["Home","About","Skills","Blog","Contact"];
   return (
   <div className='bg-blue-200'>
       <div className='container flex justify-between items-center mx-auto  px-6 py-5 '>
@@ -8,7 +9,11 @@ export default function Header() {
         <p className='text-2xl'>DR. Jamil</p>
         </div>
         <div>
-            <li>home</li>
+           {
+            navMenus.map((menu,index)=>(
+              <a className='px-5 font-semibold' key={index} >{menu}</a>
+            ))
+           }
         </div>
       </div>
   </div>
